967f0cb9948276239e6f42cf9f6bf0d720ab9388
chiwenc <adam07154211@example.com>
2023-10-04 13:15:09 +0800

    finish v1 search feature for sprint 3 demo

--- App/auth/routes.py
+++ App/auth/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, flash, redirect, url_for, request, jsonify
 from flask_login import current_user, login_user, logout_user, login_required, LoginManager
 from App.server.models.user_model import User, check_email_exist, insert_user_value, get_user_info, verify_user, insert_user_request, get_user_request, delete_user_request
-from App.server.models.hotel_model import get_request_hotel_history_price, get_hotel_all_history_price
+from App.server.models.hotel_model import get_request_hotel_history_price, get_hotel_all_history_price, search_price_history_line_chart
 from App.auth import bp
 from App.models import User
 from flask_login import UserMixin
@@ -14,13 +14,12 @@ def get_user():
     return current_user
 
 
-@bp.route("/")
-@bp.route("/index")
-@login_required
+@bp.route("/", methods=['GET','POST'])
+@bp.route("/index", methods=['GET','POST'])
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
-@bp.route("/search")
+@bp.route("/search", methods=['GET','POST'])
 def search():
     return render_template('search.html')
 
@@ -110,6 +109,14 @@ def get_dashboard_all_df():
     user_id = current_user.id
     all_data = get_hotel_all_history_price(user_id)
     return all_data
+
+@bp.route("/api/v1/hotel/search_price/", methods=['GET','POST'])
+def get_search_hotel_history_price():
+   hotel_name = request.values.get("hotel_name")
+   print(f"hotel_name {hotel_name}")
+   print(search_price_history_line_chart(hotel_name))
+   return search_price_history_line_chart(hotel_name)
+
 
 @bp.route('/logout')
 @login_required
